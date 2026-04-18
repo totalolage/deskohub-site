1694a32a8d6fda1c09fa9c920285977ee5481a49
Filip Kalný <filip@kalny.net>
2026-04-18 17:06:05 +0200

    portal: add vercel configs

--- apps/deskohub-portal/astro.config.ts
+++ apps/deskohub-portal/astro.config.ts
@@ -2,6 +2,11 @@ import vercel from "@astrojs/vercel";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  adapter: vercel(),
+  adapter: vercel({
+    imageService: true,
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
   output: "server",
 });
