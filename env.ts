3506b255f4f4c90d72839de0f9e2bce93ea25f4e
Filip Kalný <filip@kalny.net>
2026-05-28 20:19:00 +0200

    refactor(workspace): Use Vercel env for testing access

--- env.ts
+++
@@ -1,9 +0,0 @@
-export {
-  type BoardgameBarClientEnv,
-  type BoardgameBarPublicEnv,
-  type BoardgameBarServerEnv,
-  boardgameBarClientSchema,
-  boardgameBarServerSchema,
-  env,
-  publicEnv,
-} from "./apps/deskohub-boardgame-bar/env";
